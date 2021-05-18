276ad7b127bf2fb0e90ab8dced388bea3545a7c9
MarkWengSTR <bskin0330@gmail.com>
2021-05-18 23:44:38 +0800

    Vue example from youtube
    https://www.youtube.com/watch?v=oWt4jYThJCo&t=612s

---
+++ shopping-cart/src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+  {
+    id: 1,
+    name: 'Comfy Slippers',
+    price: 12.00,
+    category: 'Footwear',
+    description: 'fsgsg  fwegwe efssfdgfds  gsdgs a gdsagsagsh agfdhdfhsdfhd gshdqfhadfa hfdahdf'
+  },
+  {
+    id: 2,
+    name: 'lollf lfsdfsrs',
+    price: 45.00,
+    category: 'Footwear',
+    description: 'fvdsds   dsgs asdfsgs sdgfsgdsdasdgfs fafd  sdgsdg fhsdfhd gshdqfhad'
+  },
+  {
+    id: 3,
+    name: 'John leagen',
+    price: 100.00,
+    category: 'Star',
+    description: 'fvsgsdgfdsfs asdgsdgs dsdffddddd dssss dgsgdsgsdg  fhsdfh'
+  },
+  {
+    id: 4,
+    name: 'Hhhh',
+    price: 5000.00,
+    category: 'Jacket',
+    description: 'fvdsds   dsgs asdfsgs sdgfsgdsdasdgfs fafd  sdgsdg fhsdfhd gshdqfhadfa hfdahdf fdsgsdagash sdgsags'
+  },
+  {
+    id: 5,
+    name: 'MarkMark',
+    price: 6000000.00,
+    category: 'Star',
+    description: 'fvdsds   dsgs asdfsgs sdgfsgdsdasdgfs fafd  sdgsdg fhsdfhd gshdqfhadfa hfdahdf sdgsgd dsgsgsadsgsgs fsgsgsagsg gsgaghahsagh hfshadg08erjghg'
+  },
+]
+
+export default items
---
+++ shopping-cart/src/views/Cart.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is the cart page</h1>
+  </div>
+</template>
